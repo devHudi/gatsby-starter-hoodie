19072a4656a74b4a3554a7c8fa3b2babf8d0ad7e
Hudi <devhudi@gmail.com>
2020-11-25 00:44:57 +0900

    feature(reset.css): apply reset style

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,17 +1,23 @@
 import React from "react"
+import { createGlobalStyle } from "styled-components"
+import reset from "styled-reset"
 
 import Header from "./Header"
 import Body from "./Body"
 import Footer from "./Footer"
 
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+`
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <>
+      <GlobalStyles />
       <Header />
       <Body>{children}</Body>
       <Footer />
-    </div>
+    </>
   )
 }
 
