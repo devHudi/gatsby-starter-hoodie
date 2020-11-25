3c1e15bab3698199aa64b3d11b20d4b621cfea61
Hudi <devhudi@gmail.com>
2020-11-26 00:59:28 +0900

    feat: add transition on reveal

--- src/components/RevealOnScroll/index.jsx
+++ src/components/RevealOnScroll/index.jsx
@@ -1,20 +1,27 @@
 import React from "react"
-import { useSpring, animated } from "react-spring"
+import styled, { css } from "styled-components"
 
 import useScroll from "hooks/useScroll"
+
+const StyledWrapper = styled.div`
+  position: relative;
+  top: 30px;
+  opacity: 0;
+  transition: 0.35s all ease;
+  ${props =>
+    props.visible &&
+    css`
+      top: 0;
+      opacity: 1;
+    `}
+`
 
 const RevealOnScroll = ({ revealAt, children }) => {
   const { y } = useScroll()
 
   const reveal = y > revealAt
 
-  const props = useSpring({
-    opacity: reveal ? 1 : 0,
-    from: { opacity: 0 },
-    config: { duration: 250 },
-  })
-
-  return <animated.div style={props}>{children}</animated.div>
+  return <StyledWrapper visible={reveal}>{children}</StyledWrapper>
 }
 
 export default RevealOnScroll
