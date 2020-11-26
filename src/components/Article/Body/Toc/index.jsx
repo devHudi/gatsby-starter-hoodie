42614af7d235c8ab1898ee118ba9007f9393f4bd
Hudi <devhudi@gmail.com>
2020-11-27 00:23:25 +0900

    feat: improve style of Toc

--- src/components/Article/Body/Toc/index.jsx
+++ src/components/Article/Body/Toc/index.jsx
@@ -12,11 +12,10 @@ const TocWrapper = styled.div`
   left: 100%;
 
   & > div {
-    margin-left: 3rem;
+    margin-left: 4rem;
     position: relative;
     width: 15rem;
-    line-height: 1.6;
-    font-size: 0.9rem;
+    line-height: 1.9;
 
     ${props =>
       props.stick &&
@@ -27,10 +26,22 @@ const TocWrapper = styled.div`
   }
 `
 
-const Title = styled.div``
+const Title = styled.div`
+  font-size: 0.9rem;
+  color: #adb5bd;
+  padding-left: ${props => (props.subtitle ? 0.7 : 0)}rem;
+  transition: all 0.2s;
 
-const Subtitle = styled.div`
-  padding-left: 0.7rem;
+  ${props =>
+    props.active &&
+    css`
+      color: #495057;
+    `}
+
+  &:hover {
+    color: #212529;
+    cursor: pointer;
+  }
 `
 
 const Toc = ({ items, articleOffset }) => {
@@ -45,10 +56,9 @@ const Toc = ({ items, articleOffset }) => {
     <RevealOnScroll revealAt={revealAt} reverse>
       <TocWrapper stick={y > articleOffset - STICK_OFFSET}>
         <div>
-          {items.map(item => {
-            if (item.tagName === "H2") return <Title> {item.innerText} </Title>
-            else return <Subtitle> {item.innerText} </Subtitle>
-          })}
+          {items.map(item => (
+            <Title subtitle={item.tagName !== "H2"}> {item.innerText} </Title>
+          ))}
         </div>
       </TocWrapper>
     </RevealOnScroll>
