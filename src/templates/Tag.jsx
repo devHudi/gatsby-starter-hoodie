d3232a6002fa8885a03f8661476ed0d4bd4f68d9
Hudi <devhudi@gmail.com>
2020-11-28 23:08:28 +0900

    feat: use PostList in Tag template

--- src/templates/Tag.jsx
+++ src/templates/Tag.jsx
@@ -1,61 +1,33 @@
 import React from "react"
-import PropTypes from "prop-types"
+import styled from "styled-components"
 
 import { Link, graphql } from "gatsby"
 
 import Layout from "components/Layout"
+import PostList from "components/PostList"
+
+const Header = styled.h1`
+  margin-bottom: 2rem;
+  font-size: 1.2rem;
+  font-weight: 700;
+`
 
 const Tags = ({ pageContext, data }) => {
   const { tag } = pageContext
-  const { edges, totalCount } = data.allMarkdownRemark
-  const tagHeader = `${totalCount} post${
-    totalCount === 1 ? "" : "s"
-  } tagged with "${tag}"`
+  const { totalCount } = data.allMarkdownRemark
+  const posts = data.allMarkdownRemark.nodes
 
   return (
     <Layout>
-      <h1>{tagHeader}</h1>
-      <ul>
-        {edges.map(({ node }) => {
-          const { slug } = node.fields
-          const { title } = node.frontmatter
-          return (
-            <li key={slug}>
-              <Link to={slug}>{title}</Link>
-            </li>
-          )
-        })}
-      </ul>
-      {/*
-              This links to a page that does not yet exist.
-              You'll come back to it!
-            */}
+      <Header>
+        #{tag} 에 해당하는 글이 {totalCount}개 있습니다.
+      </Header>
+
+      <PostList postList={posts} />
+
       <Link to="/tags">All tags</Link>
     </Layout>
   )
-}
-
-Tags.propTypes = {
-  pageContext: PropTypes.shape({
-    tag: PropTypes.string.isRequired,
-  }),
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      totalCount: PropTypes.number.isRequired,
-      edges: PropTypes.arrayOf(
-        PropTypes.shape({
-          node: PropTypes.shape({
-            frontmatter: PropTypes.shape({
-              title: PropTypes.string.isRequired,
-            }),
-            fields: PropTypes.shape({
-              slug: PropTypes.string.isRequired,
-            }),
-          }),
-        }).isRequired
-      ),
-    }),
-  }),
 }
 
 export default Tags
@@ -68,14 +40,16 @@ export const pageQuery = graphql`
       filter: { frontmatter: { tags: { in: [$tag] } } }
     ) {
       totalCount
-      edges {
-        node {
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-          }
+      nodes {
+        excerpt(pruneLength: 200)
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          update(formatString: "MMM DD, YYYY")
+          title
+          tags
         }
       }
     }
