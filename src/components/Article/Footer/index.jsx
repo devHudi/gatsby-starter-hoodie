62fc5e5e1dc8297b2c7d8042e0415af9fd4494be
Hudi <devhudi@gmail.com>
2020-11-29 00:00:50 +0900

    feat: separate Bio from Footer of Article

--- src/components/Article/Footer/index.jsx
+++ src/components/Article/Footer/index.jsx
@@ -2,101 +2,10 @@ import React from "react"
 import styled from "styled-components"
 import ReactUtterences from "react-utterances"
 
-import { FaGithub, FaFacebook, FaLinkedin, FaInstagram } from "react-icons/fa"
-
-import { description, author, links, utterances } from "../../../../blog-config"
+import { utterances } from "../../../../blog-config"
 
 import Divider from "components/Divider"
-
-const FooterWrapper = styled.div``
-
-const BioWrapper = styled.div`
-  display: flex;
-  align-items: center;
-`
-
-const Profile = styled.div`
-  flex: 0 0 auto;
-  margin-right: 1rem;
-  width: 8rem;
-  height: 8rem;
-  border-radius: 999px;
-  background-image: url("https://media.vlpt.us/profiles/cdhnet98/thumbnails/1537611812.205.png?w=240");
-  background-size: cover;
-  background-position: center;
-`
-
-const Author = styled.div`
-  margin-bottom: 0.3rem;
-  font-size: 1.5rem;
-  font-weight: 700;
-  color: #212529;
-`
-
-const Description = styled.div`
-  margin-bottom: 0.7rem;
-  line-height: 1.5;
-  font-size: 1rem;
-  color: #343a40;
-`
-
-const LinksWrapper = styled.div`
-  & a {
-    margin-right: 0.6rem;
-  }
-
-  & svg {
-    width: 1.6rem;
-    height: 1.6rem;
-    cursor: pointer;
-  }
-
-  & svg path {
-    fill: #868e96;
-    transition: fill 0.3s;
-  }
-
-  & a:hover svg path {
-    fill: #212529;
-  }
-`
-
-const Link = ({ link, children }) => {
-  if (!link) return null
-  return (
-    <a href={link} target="_blank">
-      {children}
-    </a>
-  )
-}
-
-const Bio = () => {
-  const { github, instagram, facebook, linkedIn } = links
-
-  return (
-    <BioWrapper id="bio">
-      <Profile />
-      <div>
-        <Author>@{author}</Author>
-        <Description>{description}</Description>
-        <LinksWrapper>
-          <Link link={github}>
-            <FaGithub />
-          </Link>
-          <Link link={instagram}>
-            <FaInstagram />
-          </Link>
-          <Link link={facebook}>
-            <FaFacebook />
-          </Link>
-          <Link link={linkedIn}>
-            <FaLinkedin />
-          </Link>
-        </LinksWrapper>
-      </div>
-    </BioWrapper>
-  )
-}
+import Bio from "components/Bio"
 
 const Comment = () => (
   <ReactUtterences repo={utterances.repo} type={utterances.type} />
@@ -104,11 +13,11 @@ const Comment = () => (
 
 const Footer = () => {
   return (
-    <FooterWrapper>
+    <div>
       <Bio />
       <Divider mt="2rem" />
       <Comment />
-    </FooterWrapper>
+    </div>
   )
 }
 
