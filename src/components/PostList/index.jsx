06401751711740c7dade54da5c245cce0b706c96
Hudi <devhudi@gmail.com>
2020-11-28 22:43:41 +0900

    feat: show post list in main page with PostList

---
+++ src/components/PostList/index.jsx
@@ -0,0 +1,103 @@
+import React from "react"
+import styled from "styled-components"
+
+import { Link } from "gatsby"
+
+import Divider from "components/Divider"
+import InvertLinkMixin from "components/Mixins/InvertLinkMixin"
+
+const PostListWrapper = styled.div``
+
+const PostWrapper = styled.div`
+  position: relative;
+  top: 0;
+  /* left: 0; */
+  transition: all 0.5s;
+
+  &:hover {
+    /* top: -10px; */
+    /* left: 15px; */
+  }
+`
+
+const Title = styled.h1`
+  margin-bottom: 1.5rem;
+  line-height: 1.3;
+  font-size: 2.1rem;
+  font-weight: 700;
+
+  & > a {
+    text-decoration: none;
+    color: inherit;
+    transition: all 0.2s;
+  }
+
+  & > a:hover {
+    color: #495057;
+  }
+`
+
+const Date = styled.p`
+  margin-bottom: 1rem;
+  font-size: 0.9rem;
+  color: #868e96;
+`
+
+const Excerpt = styled.p`
+  margin-bottom: 2rem;
+  line-height: 1.7;
+  font-size: 1rem;
+  color: #495057;
+`
+
+const Tags = styled.div`
+  margin-bottom: 1rem;
+
+  & a {
+    padding: 0.3rem 0.7rem;
+    margin-right: 0.5rem;
+    border-radius: 50px;
+    background-color: #f1f3f5;
+    text-decoration: none;
+    font-size: 0.9rem;
+    color: #495057;
+    transition: all 0.2s;
+  }
+
+  & a:hover {
+    background-color: #dee2e6;
+  }
+`
+
+const PostList = ({ postList }) => {
+  return (
+    <PostListWrapper>
+      {postList.map((post, i) => {
+        const { title, date, tags } = post.frontmatter
+        const { excerpt } = post
+        const { slug } = post.fields
+
+        return (
+          <>
+            <PostWrapper>
+              <Title>
+                <Link to={slug}>{title}</Link>
+              </Title>
+              <Date>{date}</Date>
+              <Excerpt>{excerpt}</Excerpt>
+              <Tags>
+                {tags.map((tag, i) => (
+                  <Link to={`/tags/${tag}`}>#{tag}</Link>
+                ))}
+              </Tags>
+            </PostWrapper>
+
+            {postList.length - 1 !== i && <Divider mt="3rem" mb="2rem" />}
+          </>
+        )
+      })}
+    </PostListWrapper>
+  )
+}
+
+export default PostList
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Layout from "components/Layout"
+import PostList from "components/PostList"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
@@ -19,43 +20,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout>
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
-
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>
-                    {post.frontmatter.date}{" "}
-                    {post.frontmatter.update && (
-                      <>(Updated at {post.frontmatter.update})</>
-                    )}
-                  </small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
+      <PostList postList={posts} />
     </Layout>
   )
 }
@@ -71,7 +36,7 @@ export const pageQuery = graphql`
     }
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       nodes {
-        excerpt
+        excerpt(pruneLength: 200)
         fields {
           slug
         }
@@ -79,6 +44,7 @@ export const pageQuery = graphql`
           date(formatString: "MMMM DD, YYYY")
           update(formatString: "MMM DD, YYYY")
           title
+          tags
         }
       }
     }
