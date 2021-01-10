556d9d40123ddcfbf44fe82952d880e901b54abe
Hudi <devhudi@gmail.com>
2021-01-11 02:58:11 +0900

    feat: implement TextField

---
+++ src/components/TextField/index.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export default styled.input.attrs({ type: "text" })`
+  padding: 0.7rem 1.2rem;
+  width: 100%;
+  border: 1px solid #ced4da;
+  border-radius: 50vh;
+  background-color: transparent;
+  font-size: 1.2rem;
+  outline: none;
+  box-sizing: border-box;
+`
