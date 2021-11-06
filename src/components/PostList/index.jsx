a7196038d073b26f05a5dcc27fe9e4d0a4c7c1ff
devHudi <devhudi@gmail.com>
2021-11-07 02:32:12 +0900

    fix: fix infinite scrolling issue on mobile environment

--- src/components/PostList/index.jsx
+++ src/components/PostList/index.jsx
@@ -39,22 +39,11 @@ const Excerpt = styled.p`
 `
 
 const checkIsScrollAtBottom = () => {
-  const windowHeight =
-    "innerHeight" in window
-      ? window.innerHeight
-      : document.documentElement.offsetHeight
-  const body = document.body
-  const html = document.documentElement
-  const docHeight = Math.max(
-    body.scrollHeight,
-    body.offsetHeight,
-    html.clientHeight,
-    html.scrollHeight,
-    html.offsetHeight
+  return (
+    document.documentElement.scrollHeight -
+      document.documentElement.scrollTop <=
+    document.documentElement.clientHeight + 100
   )
-  const windowBottom = windowHeight + window.pageYOffset
-
-  return windowBottom >= docHeight
 }
 
 const PostList = ({ postList }) => {
