c1c5ff63c6487b0fd4ce86383ae6e0357a7fbab4
devHudi <devhudi@gmail.com>
2021-11-08 12:40:02 +0900

    feat: implement series page

---
+++ src/components/SeriesList/index.jsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react"
+import styled from "styled-components"
+import _ from "lodash"
+
+import { Link } from "gatsby"
+
+import Title from "components/Title"
+import Divider from "components/Divider"
+
+const SeriesListWrapper = styled.div`
+  margin-bottom: 60px;
+  @media (max-width: 768px) {
+    padding: 0 10px;
+  }
+`
+
+const SeriesWrapper = styled.div`
+  position: relative;
+  top: 0;
+  transition: all 0.5s;
+
+  @media (max-width: 768px) {
+    padding: 0 5px;
+  }
+`
+
+const SeriesInform = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${props => props.theme.colors.tertiaryText};
+
+  & > span {
+    margin: 0 5px;
+  }
+`
+
+const Date = styled.p`
+  font-size: 0.9rem;
+`
+
+const PostCount = styled.p`
+  font-size: 0.9rem;
+`
+
+const checkIsScrollAtBottom = () => {
+  return (
+    document.documentElement.scrollHeight -
+      document.documentElement.scrollTop <=
+    document.documentElement.clientHeight + 100
+  )
+}
+
+const SeriesList = ({ seriesList }) => {
+  const [seriesCount, setSeriesCount] = useState(10)
+
+  const handleMoreLoad = _.throttle(() => {
+    if (checkIsScrollAtBottom() && seriesCount < seriesList.length) {
+      setTimeout(() => setSeriesCount(seriesCount + 10), 300)
+    }
+  }, 250)
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleMoreLoad)
+
+    return () => {
+      window.removeEventListener("scroll", handleMoreLoad)
+    }
+  }, [seriesCount, seriesList])
+
+  useEffect(() => {
+    setSeriesCount(10)
+  }, [seriesList])
+
+  return (
+    <SeriesListWrapper>
+      {seriesList.slice(0, seriesCount).map((series, i) => {
+        return (
+          <>
+            <SeriesWrapper>
+              <Title size="bg">
+                <Link to={`${series.posts[series.posts.length - 1].slug}`}>
+                  {series.name}
+                </Link>
+              </Title>
+              <SeriesInform>
+                <PostCount>{series.posts.length} Posts</PostCount>
+                <span>·</span>
+                <Date>Last updated on {series.lastUpdated}</Date>
+              </SeriesInform>
+            </SeriesWrapper>
+
+            {seriesCount - 1 !== i && seriesList.length - 1 !== i && (
+              <Divider mt="3rem" mb="2rem" />
+            )}
+          </>
+        )
+      })}
+    </SeriesListWrapper>
+  )
+}
+
+export default SeriesList
---
+++ src/pages/series.jsx
@@ -0,0 +1,88 @@
+import React from "react"
+import { flow, map, groupBy, sortBy, filter } from "lodash/fp"
+import styled from "styled-components"
+import SEO from "components/SEO"
+
+import { graphql } from "gatsby"
+
+import Layout from "components/Layout"
+import Title from "components/Title"
+import SeriesList from "components/SeriesList"
+import VerticleSpace from "components/VerticalSpace"
+import NoContent from "components/NoContent"
+
+import { title, description, siteUrl } from "../../blog-config"
+
+const TagListWrapper = styled.div`
+  margin-top: 20px;
+
+  @media (max-width: 768px) {
+    padding: 0 15px;
+  }
+`
+
+const SeriesPage = ({ data }) => {
+  const posts = data.allMarkdownRemark.nodes
+  const series = flow(
+    map(post => ({ ...post.frontmatter, slug: post.fields.slug })),
+    groupBy("series"),
+    map(series => ({
+      name: series[0].series,
+      posts: series,
+      lastUpdated: series[0].date,
+    })),
+    sortBy(series => new Date(series.lastUpdated)),
+    filter(series => series.name)
+  )(posts)
+
+  console.log(series, posts)
+
+  return (
+    <Layout>
+      <SEO title={title} description={description} url={siteUrl} />
+
+      <TagListWrapper>
+        {series.length > 0 && (
+          <Title size="sm">There are {series.length} series.</Title>
+        )}
+      </TagListWrapper>
+
+      {series.length === 0 && <NoContent name="series" />}
+
+      <VerticleSpace size={2} />
+
+      <SeriesList seriesList={series} />
+    </Layout>
+  )
+}
+
+export default SeriesPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+      nodes {
+        excerpt(pruneLength: 200, truncate: true)
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          update(formatString: "MMM DD, YYYY")
+          title
+          tags
+          series
+        }
+      }
+    }
+  }
+`
