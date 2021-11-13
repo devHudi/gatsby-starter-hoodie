9cd859edff140cfeb7c7d615b9defa5f567a8683
devHudi <devhudi@gmail.com>
2021-11-13 19:36:30 +0900

    feat: move path of images

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#ced4da`,
         display: `standalone`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/favicon.png`,
       },
     },
     {
