18645cc01c9060cc347af2aba17af91a60f02b37
devHudi <devhudi@gmail.com>
2021-12-26 00:56:45 +0900

    chore: update blog-config.js

--- blog-config.js
+++ blog-config.js
@@ -5,12 +5,10 @@ module.exports = {
   siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
   links: {
     github: "https://github.com/devHudi",
-    kaggle: "https://kaggle.com",
     linkedIn: "https://linkedin.com",
-    facebook: "https://www.facebook.com/profile.php?id=100057724153835",
-    instagram: "https://www.instagram.com/dawn_fromeast/",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com",
     email: "mailto:devhudi@gmail.com",
-    etc: "https://www.notion.so/Hudi-s-R-sum-0c1d1a1b35284d1eaf05c5bfac4a3cad",
   },
   utterances: {
     repo: "devHudi/gatsby-starter-hoodie",
