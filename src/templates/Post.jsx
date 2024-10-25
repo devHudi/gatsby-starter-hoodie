5bb93ef2db39a7989170b1b96f6c37065c3457f9
ZungTa <quddnr0708@gmail.com>
2024-10-25 16:24:24 +0900

    fix: graphql query

--- src/templates/Post.jsx
+++ src/templates/Post.jsx
@@ -86,7 +86,7 @@ export const pageQuery = graphql`
       }
     }
     seriesList: allMarkdownRemark(
-      sort: { order: ASC, fields: [frontmatter___date] }
+      sort: { frontmatter: { date: ASC } }
       filter: { frontmatter: { series: { eq: $series } } }
     ) {
       edges {
--- src/templates/Series.jsx
+++ src/templates/Series.jsx
@@ -90,7 +90,7 @@ export default Series
 export const pageQuery = graphql`
   query BlogSeriesBySeriesName($series: String) {
     posts: allMarkdownRemark(
-      sort: { order: ASC, fields: [frontmatter___date] }
+      sort: { frontmatter: { date: ASC } }
       filter: { frontmatter: { series: { eq: $series } } }
     ) {
       nodes {
